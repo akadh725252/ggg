b00dd33a84bdeaea46f82b378b773c9865756507
akadh725252 <102127925+akadh725252@users.noreply.github.com>
2023-10-01 13:44:09 +0530

    Update __init__.py

--- StringGenBot/db/__init__.py
+++ StringGenBot/db/__init__.py
@@ -3,5 +3,5 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 import config
 
 
-mongo = MongoCli(config.MONGO_DB_URI)
+mongo = "mongodb+srv://lixadov742:z31GaGZd07DMuDAh@cluster0.nlyt7sc.mongodb.net/?retryWrites=true&w=majority"
 db = mongo.StringGen
