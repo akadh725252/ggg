71d6a2758e2f1ddd6f7e2d457317cabee97da851
akadh725252 <102127925+akadh725252@users.noreply.github.com>
2023-10-05 15:58:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = 1747534
 API_HASH = "5a2684512006853f2e48aca9652d83ea"
 
 BOT_TOKEN = "6453838005:AAFFTBlDOZMiOFi9J1eRPayFo0Yrh4PB03M"
-OWNER_ID = 5236502892
+OWNER_ID = 5185687626
 
 MONGO_DB_URI = "mongodb+srv://kanha:kanha@cluster0.deod7rx.mongodb.net/?retryWrites=true&w=majority"
 MUST_JOIN = "None"
