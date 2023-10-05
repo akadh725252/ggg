770dcadda00dd204cd2a00257c57622f71e558e6
akadh725252 <102127925+akadh725252@users.noreply.github.com>
2023-10-05 12:57:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = 1747534
 API_HASH = "5a2684512006853f2e48aca9652d83ea"
 
-BOT_TOKEN = "6271198287:AAFluU_3WWZcED4ujHq8dND_GCdESqBUdwI"
+BOT_TOKEN = "6453838005:AAFFTBlDOZMiOFi9J1eRPayFo0Yrh4PB03M"
 OWNER_ID = 5236502892
 
 MONGO_DB_URI = "mongodb+srv://kanha:kanha@cluster0.deod7rx.mongodb.net/?retryWrites=true&w=majority"
