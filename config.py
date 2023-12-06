51a8fbff96e5dd7c8341ca7f3a4473c76cbcc004
akadh725252 <102127925+akadh725252@users.noreply.github.com>
2023-12-06 13:47:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ API_HASH = "5a2684512006853f2e48aca9652d83ea"
 BOT_TOKEN = "6453838005:AAFFTBlDOZMiOFi9J1eRPayFo0Yrh4PB03M"
 OWNER_ID = 5185687626
 
-MONGO_DB_URI = "mongodb+srv://kanha:kanha@cluster0.deod7rx.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://kanha:kanha@cluster0.ofwoggc.mongodb.net/?retryWrites=true&w=majority
 MUST_JOIN = "None"
